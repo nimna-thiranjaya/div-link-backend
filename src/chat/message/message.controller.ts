1150c8962087f9d4c9a57f082ccbc68594732604
nimna-thiranjaya <nimnathiranjaya523@gmail.com>
2023-08-29 22:34:11 +0530

    message model

--- src/chat/message/message.controller.ts
+++ src/chat/message/message.controller.ts
@@ -1,1 +1,26 @@
 import mongoose from "mongoose";
+
+const MessageSchema = new mongoose.Schema(
+  {
+    conversation: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Conversation",
+    },
+
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    content: {
+      type: String,
+      required: [true, "Content is required"],
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export default mongoose.model("Message", MessageSchema);
