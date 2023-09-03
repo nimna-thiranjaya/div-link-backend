f2580a68691fc19568dc9588fc5438a4e718d169
nimna-thiranjaya <nimnathiranjaya523@gmail.com>
2023-09-03 15:49:58 +0530

    setup test cron to deploy

--- src/util/cronJob.ts
+++ src/util/cronJob.ts
@@ -1,4 +1,5 @@
 import schedule from "node-schedule";
+import { sendEmail } from "./emailServer";
 
 //cron job helper fun
 export const cronJob = (cronTime: string, callback: () => void) => {
@@ -8,7 +9,14 @@ export const cronJob = (cronTime: string, callback: () => void) => {
 //send appointment reminders daily at 6 am
 const sendAppointmentReminders = () => {
   cronJob("0 6 * * *", () => {
-    console.log("Cron job executed daily at 6 am:", new Date());
+    console.log("Cron job executed daily at 6 am:", async () => {
+      await sendEmail(
+        "lavanpasindu123@gmail.com",
+        "Appointment Reminder",
+        `<h1>Appointment Reminder</h1>`,
+        null
+      );
+    });
   });
 };
 
