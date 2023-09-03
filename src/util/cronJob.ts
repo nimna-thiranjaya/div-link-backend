d7d6e58f809f5cfbd9efaef4d1e46a569606bb86
nimna-thiranjaya <nimnathiranjaya523@gmail.com>
2023-09-03 14:49:20 +0530

    bug fix

--- src/util/cronJob.ts
+++ src/util/cronJob.ts
@@ -1,13 +1,13 @@
 import schedule from "node-schedule";
 
-//cron job helper function
+//cron job helper fun
 export const cronJob = (cronTime: string, callback: () => void) => {
   schedule.scheduleJob(cronTime, callback);
 };
 
-//cron job to send appointment reminders daily at 6 am
+//send appointment reminders daily at 6 am
 const sendAppointmentReminders = () => {
-  const job = schedule.scheduleJob("0 6 * * *", function () {
+  cronJob("0 6 * * *", () => {
     console.log("Cron job executed daily at 6 am:", new Date());
   });
 };
